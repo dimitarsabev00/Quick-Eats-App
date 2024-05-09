4c73ba9ff8cb407bce37eb0b223cdeafca0e21f1
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-09 23:06:49 +0300

    Added Protected Route Logic For Routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,11 +1,43 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation } from "react-router";
 import { Auth, Main } from "../screens";
+import { auth } from "../configs/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { ReactNode } from "react";
 
 const RoutesComp = () => {
+  const [user] = useAuthState(auth);
+
+  const PrivateRoute = ({ children }: { children: ReactNode }) => {
+    let location = useLocation();
+    if (!user)
+      return <Navigate to="/auth" state={{ from: location }} replace />;
+    return children;
+  };
+
+  const AuthRoute = ({ children }: { children: ReactNode }) => {
+    let location = useLocation();
+    if (user) return <Navigate to={"/"} state={{ from: location }} replace />;
+    return children;
+  };
+
   return (
     <Routes>
-      <Route path="/*" element={<Main />} />
-      <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <Main />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/auth"
+        element={
+          <AuthRoute>
+            <Auth />
+          </AuthRoute>
+        }
+      />
     </Routes>
   );
 };
