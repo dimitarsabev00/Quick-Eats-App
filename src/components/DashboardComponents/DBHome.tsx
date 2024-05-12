e3cba2c15128b8d6751cb920907fd1a1973e423a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-12 23:05:38 +0300

    Get All Products When Open Dashboard Screen

--- src/components/DashboardComponents/DBHome.tsx
+++ src/components/DashboardComponents/DBHome.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { collection, getDocs, query } from "@firebase/firestore";
+import { db } from "../../configs/firebase";
+import { setAllProducts } from "../../store";
 
 const DBHome: React.FC = () => {
+  const products = useAppSelector(({ generalSlice }) => generalSlice.products);
+  const dispatch = useAppDispatch();
+
+  const getAllProducts = async () => {
+    // Fetch all products from Firestore
+
+    const productsQuery = query(collection(db, "products"));
+    const querySnapshot = await getDocs(productsQuery);
+    const allProducts = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    dispatch(setAllProducts(allProducts));
+  };
+
+  useEffect(() => {
+    if (!products.length) {
+      getAllProducts();
+    }
+  }, []);
+
   return (
     <div className="flex items-center justify-center flex-col pt-6 w-full h-full">
       DBHome
