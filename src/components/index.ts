9371d5f060a8305c38fa0959806e3a41ebf1a9e0
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-12 22:19:51 +0300

    Added Loader Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Header } from "./Header";
 export { default as DBLeftSection } from "./DashboardComponents/DBLeftSection";
 export { default as DBRightSection } from "./DashboardComponents/DBRightSection";
 export { default as DataTable } from "./DataTable";
+export { default as Loader } from "./Loader";
