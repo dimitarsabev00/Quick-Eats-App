aa3d1f85874951a071738da99aad7204b52e0967
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-17 10:09:38 +0300

    Added Orders Design In Dashboard Screen

--- client/src/components/DashboardComponents/DBOrders.tsx
+++ client/src/components/DashboardComponents/DBOrders.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import OrdersData from "./OrdersData";
 
 const DBOrders: React.FC = () => {
+  const orders = useAppSelector(({ generalSlice }) => generalSlice.orders);
+  const dispatch = useAppDispatch();
+
+  const getAllOrders = async () => {
+    // Fetch all orders here from DB
+  };
+
+  useEffect(() => {
+    if (!orders.length) {
+      getAllOrders();
+    }
+  }, []);
+
+  console.log(orders);
+
   return (
     <div className=" flex items-center justify-center flex-col pt-6 w-full gap-4">
-      DBOrders
+      {orders.length ? (
+        <>
+          {orders.map((item, i) => (
+            <OrdersData key={i} index={i} data={item} admin={true} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h1 className="text-[72px] text-headingColor font-bold">No Orders</h1>
+        </>
+      )}
     </div>
   );
 };
--- client/src/store/slices/generalSlice.ts
+++ client/src/store/slices/generalSlice.ts
@@ -14,6 +14,7 @@ const initialState: GeneralSliceInitialState = {
   allUsers: [],
   shoppingCart: [],
   isShoppingCartVisible: false,
+  orders:[]
 };
 
 export const generalSlice = createSlice({
