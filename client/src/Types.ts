8a6f145b08e2b0c9a3e5d179c480e01e61ec15af
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-17 10:06:38 +0300

    Added Order Interface

--- client/src/Types.ts
+++ client/src/Types.ts
@@ -5,8 +5,13 @@ export interface GeneralSliceInitialState {
   allUsers: User[];
   shoppingCart: ShoppingCartProduct[];
   isShoppingCartVisible: boolean;
+  orders: Order[];
 }
 
+export interface Order {
+  orderId:number
+  // write type fields when im ready with create orders in app
+}
 export interface User {
   createdAt: number;
   uid: number;
@@ -27,5 +32,5 @@ export interface ShoppingCartProduct {
   product_category: string;
   product_price: number;
   productId: string;
-  quantity:number;
+  quantity: number;
 }
