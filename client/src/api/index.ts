bbf3e5f7da9b5b05ecaf2b6c7685dc6081b63c2a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-18 20:19:14 +0300

    Added addNewProduct & getAllProducts Requests In api Folder In Front-End

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { Product } from "../Types";
 
 export const baseURL =
   "http://localhost:5001/quick-eats-app-1bce5/us-central1/app";
@@ -10,6 +11,28 @@ export const validateUserJWTToken = async (token: string) => {
       headers: { Authorization: "Bearer " + token },
     });
     return res;
+  } catch (err: any) {
+    toast.error(err);
+  }
+};
+
+// add new product
+
+export const addNewProduct = async (data: Product) => {
+  try {
+    const res = await axios.post(`${baseURL}/api/products/create`, { ...data });
+    return res.data.data;
+  } catch (err: any) {
+    toast.error(err);
+  }
+};
+
+// get all products
+
+export const getAllProducts = async () => {
+  try {
+    const res = await axios.get(`${baseURL}/api/products/all`);
+    return res.data.data;
   } catch (err: any) {
     toast.error(err);
   }
