8da3c2d660daa9b695d438459dec8287b3a304e0
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-27 22:10:13 +0300

    Added 'swiper/css/navigation' In main.tsx File In Root Level In Application(Front-End)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import "./main.css";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
 import "swiper/css";
+import 'swiper/css/navigation';
 import "swiper/css/bundle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
